fd224fd9362a3d830820d316eee7e03587f9efb0
Purushottam Kumar <purushottam.kumar@janitri.in>
2023-01-16 22:45:12 +0530

    new activity

---
+++ app/src/main/java/com/yesandroid/locationapp/screenone/ScreenOne.kt
@@ -0,0 +1,12 @@
+package com.yesandroid.locationapp.screenone
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.yesandroid.locationapp.R
+
+class ScreenOne : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_screen_one)
+    }
+}
---
+++ app/src/main/java/com/yesandroid/locationapp/screentwo/ScreenTwo.kt
@@ -0,0 +1,12 @@
+package com.yesandroid.locationapp.screentwo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.yesandroid.locationapp.R
+
+class ScreenTwo : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_screen_two)
+    }
+}
